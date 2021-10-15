e0ae2bc003d4ee27fd9c8246a29ce76f48974b8b
roku <roku.eth@protonmail.com>
2021-10-15 10:36:31 +0800

    fix: basic tests on setStrategy

--- test/unit-tests/strategies/deltaStrategy.ts
+++ test/unit-tests/strategies/deltaStrategy.ts
@@ -1,13 +1,26 @@
 import { ethers } from 'hardhat';
 import { DeltaStrategy } from '../../../typechain';
 import { expect } from "chai";
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { constants } from 'ethers';
 
 describe('Delta Vault Strategy', async () => {
+
+  let manager: SignerWithAddress
+  let randomUser: SignerWithAddress
+
+  let strategy: DeltaStrategy
+
+  describe('setup roles', async() => {
+    const addresses = await ethers.getSigners()
+    manager = addresses[0]
+    randomUser = addresses[9]
+  })
 
   describe('deployment', async() => {
     it('deploy strategy', async() => {
       const DeltaStrategy = await ethers.getContractFactory("DeltaStrategy");
-      const strategy = await DeltaStrategy.deploy(ethers.constants.AddressZero, ethers.constants.AddressZero) as DeltaStrategy;
+      strategy = await DeltaStrategy.deploy(ethers.constants.AddressZero, ethers.constants.AddressZero) as DeltaStrategy;
   
       expect(await strategy.optionMarketViwer()).to.be.eq(ethers.constants.AddressZero)
       expect(await strategy.blackScholes()).to.be.eq(ethers.constants.AddressZero)
@@ -15,7 +28,17 @@ describe('Delta Vault Strategy', async () => {
   })
   
   describe('set strategy', async() => {
-    it('setting strategy should update isRoundReady and roundStrategy', async() => {})
-    it('should revert if setStrategy is not called by owner', async() => {})
+    it('setting strategy should update isRoundReady and roundStrategy', async() => {
+      const roundId = 1
+      const strategyBytes = constants.AddressZero
+      await strategy.connect(manager).setStrategy(roundId, strategyBytes)
+      expect(await strategy.isReadyForRound(roundId)).to.be.true 
+    })
+    it('should revert if setStrategy is not called by owner', async() => {
+      await expect(strategy.connect(randomUser).setStrategy(0, constants.AddressZero)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+      
+    })
   })
 })
