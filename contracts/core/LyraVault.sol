be45454f617265f1eb73ac43fd54b026f301bbb9
roku <roku.eth@protonmail.com>
2021-10-21 17:49:13 +0100

    feat: add mocked option market and strategy contract

--- contracts/core/LyraVault.sol
+++ contracts/core/LyraVault.sol
@@ -1,10 +1,39 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-contract LyraVault {
-  address public immutable lyra;
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IVaultStrategy } from "../interfaces/IVaultStrategy.sol";
+import { IOptionMarket } from "../interfaces/IOptionMarket.sol";
 
-  constructor(address _lyra) {
-    lyra = _lyra;
+/// @notice LyraVault help users run option-selling strategies on Lyra AMM.
+contract LyraVault is Ownable {
+  
+  IOptionMarket public immutable optionMarket;
+  
+  IVaultStrategy public strategy;
+
+  event StrategyUpdated(address strategy);
+
+  constructor(address _optionMarket) {
+    optionMarket = IOptionMarket(_optionMarket);
+  }
+  
+  /// @dev set strategy contract. This function can only be called by owner.
+  function setStrategy(address _strategy) external onlyOwner {
+    strategy = IVaultStrategy(_strategy);
+    emit StrategyUpdated(_strategy);
+  }
+
+  /// @dev anyone can trigger a trade
+  function trade() external {
+    
+    // get trade detail from strategy
+    (uint listingId, uint amount, uint minPremium) = strategy.requestTrade();
+
+    // open a short call position on lyra and collect premium
+    uint realPremium = optionMarket.openPosition(listingId, IOptionMarket.TradeType.SHORT_CALL, amount);
+
+    require(realPremium > minPremium, "premium too low");
+    require(strategy.checkPostTrade(), "bad trade");
   }
 }
---
+++ contracts/mocks/MockOptionMarket.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import {IOptionMarket} from "../interfaces/IOptionMarket.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MockOptionMarket {
+
+  address public token;
+  uint public cost;
+
+  function setMockCost(address _token, uint _cost) external {
+    token = _token;
+    cost = _cost;
+  }
+
+  function openPosition(uint /*_listingId*/, IOptionMarket.TradeType /*tradeType*/, uint /*amount*/) 
+    external 
+    returns (uint totalCost) 
+  {
+    IERC20(token).transferFrom(msg.sender, address(this), cost);
+    // todo: mint mocked certificate?
+    return cost;
+  }
+}
