3edc5ff2ec851796c28da83f15025b7ce11f72a8
mjs-12 <71236136+mjs-12@users.noreply.github.com>
2021-11-25 17:17:16 +1100

    weth to seth

--- contracts/core/LyraVault.sol
+++ contracts/core/LyraVault.sol
@@ -32,8 +32,8 @@ contract LyraVault is Ownable, BaseVault {
   /// @dev Synthetix currency key for sUSD
   bytes32 private immutable premiumCurrencyKey;
 
-  /// @dev Synthetix currency key for WETH
-  bytes32 private immutable wethCurrencyKey;
+  /// @dev Synthetix currency key for sETH
+  bytes32 private immutable sETHCurrencyKey;
 
   event StrategyUpdated(address strategy);
 
@@ -46,16 +46,16 @@ contract LyraVault is Ownable, BaseVault {
     string memory _tokenSymbol,
     Vault.VaultParams memory _vaultParams,
     bytes32 _premiumCurrencyKey,
-    bytes32 _wethCurrencyKey
+    bytes32 _sETHCurrencyKey
   ) BaseVault(_feeRecipient, 0, 0, _tokenName, _tokenSymbol, _vaultParams) {
     optionMarket = IOptionMarket(_optionMarket);
     synthetix = ISynthetix(_synthetix);
     IERC20(_vaultParams.asset).approve(_optionMarket, uint(-1));
 
     premiumCurrencyKey = _premiumCurrencyKey;
-    wethCurrencyKey = _wethCurrencyKey;
+    sETHCurrencyKey = _sETHCurrencyKey;
 
-    // allow synthetix to trade sUSD for WETH
+    // allow synthetix to trade sUSD for sETH
     IERC20(_susd).approve(_synthetix, uint(-1));
   }
 
@@ -85,8 +85,8 @@ contract LyraVault is Ownable, BaseVault {
 
     require(strategy.checkPostTrade(), "bad trade");
 
-    // exhcnage sUSD to WETH
-    synthetix.exchange(premiumCurrencyKey, realPremium, wethCurrencyKey);
+    // exhcnage sUSD to sETH
+    synthetix.exchange(premiumCurrencyKey, realPremium, sETHCurrencyKey);
   }
 
   /// @notice settle outstanding short positions.
